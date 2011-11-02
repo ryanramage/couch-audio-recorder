970f7da13209890cead289d27f74fc2aab695c7f
Ryan Ramage <ryan.ramage@gmail.com>
2011-11-01 22:37:07 -0600

    Better jquery plugin

--- html/js/jquery.couchaudiorecorder.js
+++ html/js/jquery.couchaudiorecorder.js
@@ -1,42 +1,284 @@
-/**
- * Created by .
- * User: ryan
- * Date: 11-07-16
- * Time: 9:30 AM
- * To change this template use File | Settings | File Templates.
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
  */
-(function( $ ){
-  $.fn.couchaudiorecorder = function(options) {
+(function( $ ) {
 
-        var element = this;
-        element.addClass('couchaudiorecorder');
 
-        if (!options.db) throw "Please provide a db";
-        var settings = {
-            launchRecorderUrl : "recorder.jnlp",
-            recordingDocumentID : null
+
+    var RecordingState = {
+        UNKNOWN : "UNKNOWN",
+        RECORDING_COMPLETE : "recordingComplete",
+        POST_PROCESSING_STARTED : "postProcessingStarted",
+        STOP_COMPLETE : "stopComplete",
+        STOP_ASKED : "stopAsked",
+        START_COMPLETE : "startComplete",
+        START_ASKED : "startAsked",
+        RECORDER_ASKED : "recorderAsked",
+        RECORDER_AVAILABLE : "recorderAvailable"
+    }
+
+
+    // The public methods
+    var methods = {
+        init : function(options) {
+            return this.each(function() {
+
+                // sort out the options
+                if (!options || !options.db) {
+                    $.error( 'please provide a db in the options' );
+                }
+                var settings = {
+                    launchRecorderUrl : "recorder.jnlp"
+                }
+
+                $.extend( settings, options );
+                
+                // get the element
+                var $this = $(this);
+                $this.addClass('couchaudiorecorder');
+                
+
+                data = $this.data('couchaudiorecorder', {
+                    element : $this,
+                    settings: settings
+                });                
+            });
+        },
+        newRecording : function(recordingOptions) {
+            var recordingSettings = {
+                _id : "com.eckoit.recording:" + new Date().getTime()
+            }
+            if ( recordingOptions ) {
+                $.extend( recordingSettings, recordingOptions );
+            }
+
+            var $this = $(this),
+                data  = $this.data('couchaudiorecorder');
+
+
+            // create the doc
+            var doc = {
+                _id : recordingSettings._id,
+                recordingState : {}
+            }
+
+            if (recordingSettings.settings) {
+                doc.settings = recordingSettings.settings;
+            }
+
+            data.settings.db.saveDoc(doc, {
+                success : function(){
+                    initRecording(doc, data);
+                }
+            })
+        },
+        loadRecording : function(recordingId) {
+
+            var $this = $(this),
+                data  = $this.data('couchaudiorecorder');
+
+
+            data.settings.db.openDoc(recordingId, {
+                success : function(doc) {
+                    console.log(data.settings);
+                    initRecording(doc, data);
+                },
+                error : function() {
+                    $.error( 'Unable to load recording: ' + recordingId );
+                }
+            })
         }
-        if ( options ) {
-            $.extend( settings, options );
-        }
-        if (!settings.recordingDocumentID) {
-            settings.recordingDocumentID = "recording-" + new Date().getTime()
-        }
+        
 
-        var RecordingState = {
-            UNKNOWN : "UNKNOWN",
-            RECORDING_COMPLETE : "recordingComplete",
-            POST_PROCESSING_STARTED : "postProcessingStarted",
-            STOP_COMPLETE : "stopComplete",
-            STOP_ASKED : "stopAsked",
-            START_COMPLETE : "startComplete",
-            START_ASKED : "startAsked",
-            RECORDER_ASKED : "recorderAsked",
-            RECORDER_AVAILABLE : "recorderAvailable"
-        }
+    }
 
 
-        function recordingStatus(doc) {
+
+    function recorderNotFound(doc, data) {
+        console.log("none found");
+        data.element.append('<p>Please start your <a href="'+data.settings.launchRecorderUrl+'">Audio Recorder Plugin</a> </p>');
+    }
+
+
+    function recorderFound(doc, data) {
+        clearTimeout(data.findRecorderTimeoutID);
+        console.log("recorder found");
+        createControlPanel(doc, data);
+    }
+
+
+    function createControlPanel(doc, data) {
+
+        var mic = $('<div class="mic-icon" ></div>');
+
+        var start =  $('<button class="start" >Start</button>');
+        var stop  =  $('<button class="stop" disabled="disabled">Stop</button>');
+        var timer =  $('<div   class="timer">00:00:00</div>');
+        var status = $('<div   class="status"></div>');
+
+        var mainDiv = $('<div></div>');
+        mainDiv.append(mic);
+        mainDiv.append(timer)
+
+        var bottomDiv = $('<div></div>');
+        bottomDiv.append(start);
+        bottomDiv.append(stop);
+
+        mainDiv.append(bottomDiv);
+        mainDiv.append(status);
+        data.element.html(mainDiv);
+
+        start.click( function() {
+            startRecording(doc, data);
+        });
+
+        stop.click(function() {
+            stopRecording(doc, data);
+        })
+
+    }
+
+    function startRecording(doc, data) {
+        doc.recordingState.startAsked = new Date().getTime();
+        data.settings.db.saveDoc(doc, {
+            success : function(){
+                // prob should start a timer here...just in case we
+                // have lost the recorder
+            }
+        })
+    }
+
+    function startRecordingConfirmed(doc, data) {
+        console.log('Start confirmed!');
+        // ok, start the timer
+        data.updateTimerDisplayID = setInterval(updateTimerDisplay, 1000, doc)
+        // change the buttons
+        $('.couchaudiorecorder button.start').attr('disabled', 'disabled');
+        $('.couchaudiorecorder button.stop').removeAttr("disabled");
+    }
+
+    var timeFormat = {
+            showHour: true,
+            showMin: true,
+            showSec: true,
+            padHour: true,
+            padMin: true,
+            padSec: true,
+            sepHour: ":",
+            sepMin: ":",
+            sepSec: ""
+    };
+
+    var convertTime = function(s) {
+            var myTime = new Date(s * 1000);
+            var hour = myTime.getUTCHours();
+            var min = myTime.getUTCMinutes();
+            var sec = myTime.getUTCSeconds();
+            var strHour = (timeFormat.padHour && hour < 10) ? "0" + hour : hour;
+            var strMin = (timeFormat.padMin && min < 10) ? "0" + min : min;
+            var strSec = (timeFormat.padSec && sec < 10) ? "0" + sec : sec;
+            return ( strHour + timeFormat.sepHour ) + ((timeFormat.showMin) ? strMin + timeFormat.sepMin : "") + ((timeFormat.showSec) ? strSec + timeFormat.sepSec : "");
+    };
+
+    function updateTimerDisplay(doc) {
+        var startTime = doc.recordingState.startComplete;
+        var now = new Date().getTime();
+        var elapsed = convertTime((now - startTime) / 1000);
+        $('.couchaudiorecorder div.timer').text(elapsed); // format me please
+    }
+
+
+    function stopRecording(doc, data) {
+        $('.couchaudiorecorder button.stop').attr('disabled', 'disabled');
+        $('.couchaudiorecorder div.status').text('Stopping...');
+        // note this doc, is probably stale.
+        doc = data.settings.db.openDoc(doc._id, {
+            success: function(doc) {
+                doc.recordingState.stopAsked = new Date().getTime();
+                data.settings.db.saveDoc(doc, {
+                    success : function() {
+                        // again, should have a timer to confirm...
+                    }
+                })
+
+
+            }
+        });
+        clearInterval(data.updateTimerDisplayID);
+    }
+
+    function stopRecordingConfirmed(doc, data) {
+
+        $('.couchaudiorecorder div.status').text('Finishing...');
+    }
+
+
+    function recordingCompleteConfirmed(doc, data) {
+        $('.couchaudiorecorder div.status').text('Recording Complete!');
+    }
+
+
+    function findRecorder(doc, data) {
+        console.log("Find recorder");
+        if (!doc.recordingState) {
+            doc.recordingState = {}
+        }
+        doc.recordingState.recorderAsked = new Date().getTime();
+        data.settings.db.saveDoc(doc, {
+            success: function() {
+                // start a timer.
+                data.findRecorderTimeoutID = setTimeout(recorderNotFound, 2000, doc, data);
+            }
+        });
+    }
+
+
+    function stateEvent(doc, state, data) {
+        console.log(state);
+        if (state == RecordingState.UNKNOWN) {
+            // lets ask for a recorder
+            findRecorder(doc, data);
+        }
+        if (state == RecordingState.RECORDER_AVAILABLE) {
+            // show the start button
+            recorderFound(doc, data);
+        }
+        if (state == RecordingState.START_COMPLETE) {
+            startRecordingConfirmed(doc, data);
+        }
+        if (state == RecordingState.STOP_COMPLETE) {
+            stopRecordingConfirmed(doc, data);
+        }
+        if (state == RecordingState.RECORDING_COMPLETE) {
+            recordingCompleteConfirmed(doc, data);
+        }
+    }
+
+
+
+    function initRecording(doc, data) {
+        // subscribe!
+        //var $changes = settings.db.changes(null, {filter : "couchaudiorecorder/recordings", id : doc._id});
+        data.settings.db.changes(null, {filter : "couchaudiorecorder/recordings", id : doc._id}).onChange(function (change) {
+            console.log('Got change: ');
+            console.log(change)
+            // get the doc
+            data.settings.db.openDoc(doc._id, {
+                success : function(doc) {
+                    console.log("got doc");
+                    var status = $.couchaudiorecorder.recordingStatus(doc);
+                    stateEvent(doc, status, data);
+                }
+            })
+        });
+        var state = $.couchaudiorecorder.recordingStatus(doc);
+        stateEvent(doc, state, data);
+    }
+
+
+    $.couchaudiorecorder = {
+        recordingStatus : function(doc) {
             var recordingState = doc.recordingState;
             if (recordingState != null) {
                 if (recordingState.recordingComplete != null) {
@@ -67,218 +309,22 @@
             }
             return RecordingState.UNKNOWN;
         }
+    };
 
-        var findRecorderTimeoutID = null;
 
-        function recorderNotFound(doc, settings) {
-            console.log("none found");
-            element.append('<p>Please start your <a href="'+settings.launchRecorderUrl+'">Audio Recorder Plugin</a> </p>');
+
+    // bind to the jQuery object, dispatch names to methods
+    $.fn.couchaudiorecorder = function(method) {
+        console.log("couchaudiorecorder");
+        console.log(method);
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + method + ' does not exist on jQuery.couchaudiorecorder');
         }
+    }
 
+}) ( jQuery )
 
-        function recorderFound(doc, settings) {
-            clearTimeout(findRecorderTimeoutID);
-            console.log("recorder found");
-            createControlPanel(doc, settings);
-        }
-
-
-        function createControlPanel(doc, settings) {
-
-            var mic = $('<div class="mic-icon" ></div>');
-
-            var start =  $('<button class="start" >Start</button>');
-            var stop  =  $('<button class="stop" disabled="disabled">Stop</button>');
-            var timer =  $('<div   class="timer">00:00:00</div>');
-            var status = $('<div   class="status"></div>');
-
-            var mainDiv = $('<div></div>');
-            mainDiv.append(mic);
-            mainDiv.append(timer)
-
-            var bottomDiv = $('<div></div>');
-            bottomDiv.append(start);
-            bottomDiv.append(stop);
-
-            mainDiv.append(bottomDiv);
-            mainDiv.append(status);
-            element.html(mainDiv);
-
-            start.click( function() {
-                startRecording(doc, settings);
-            });
-
-            stop.click(function() {
-                stopRecording(doc, settings);
-            })
-
-        }
-
-        function startRecording(doc, settings) {
-            doc.recordingState.startAsked = new Date().getTime();
-            settings.db.saveDoc(doc, {
-                success : function(){
-                    // prob should start a timer here...just in case we
-                    // have lost the recorder
-                }
-            })
-        }
-
-        var updateTimerDisplayID;
-
-        function startRecordingConfirmed(doc, settings) {
-            console.log('Start confirmed!');
-            // ok, start the timer
-            updateTimerDisplayID = setInterval(updateTimerDisplay, 1000, doc)
-            // change the buttons
-            $('.couchaudiorecorder button.start').attr('disabled', 'disabled');
-            $('.couchaudiorecorder button.stop').removeAttr("disabled");
-        }
-
-	var timeFormat = {
-		showHour: true,
-		showMin: true,
-		showSec: true,
-		padHour: true,
-		padMin: true,
-		padSec: true,
-		sepHour: ":",
-		sepMin: ":",
-		sepSec: ""
-	};
-
-	var convertTime = function(s) {
-		var myTime = new Date(s * 1000);
-		var hour = myTime.getUTCHours();
-		var min = myTime.getUTCMinutes();
-		var sec = myTime.getUTCSeconds();
-		var strHour = (timeFormat.padHour && hour < 10) ? "0" + hour : hour;
-		var strMin = (timeFormat.padMin && min < 10) ? "0" + min : min;
-		var strSec = (timeFormat.padSec && sec < 10) ? "0" + sec : sec;
-		return ( strHour + timeFormat.sepHour ) + ((timeFormat.showMin) ? strMin + timeFormat.sepMin : "") + ((timeFormat.showSec) ? strSec + timeFormat.sepSec : "");
-	};
-
-        function updateTimerDisplay(doc) {
-            var startTime = doc.recordingState.startComplete;
-            var now = new Date().getTime();
-            var elapsed = convertTime((now - startTime) / 1000);
-            $('.couchaudiorecorder div.timer').text(elapsed); // format me please
-        }
-
-
-        function stopRecording(doc, settings) {
-            $('.couchaudiorecorder button.stop').attr('disabled', 'disabled');
-            $('.couchaudiorecorder div.status').text('Stopping...');
-            // note this doc, is probably stale.
-            doc = settings.db.openDoc(doc._id, {
-                success: function(doc) {
-                    doc.recordingState.stopAsked = new Date().getTime();
-                    settings.db.saveDoc(doc, {
-                        success : function() {
-                            // again, should have a timer to confirm...
-                        }
-                    })
-
-
-                }
-            });
-            clearInterval(updateTimerDisplayID);
-        }
-
-        function stopRecordingConfirmed(doc, settings) {
-            
-            $('.couchaudiorecorder div.status').text('Finishing...');
-        }
-
-
-        function recordingCompleteConfirmed(doc, settings) {
-            $('.couchaudiorecorder div.status').text('Recording Complete!');
-        }
-
-
-        function findRecorder(doc, settings) {
-            console.log("Find recorder");
-            if (!doc.recordingState) {
-                doc.recordingState = {}
-            }
-            doc.recordingState.recorderAsked = new Date().getTime();
-            settings.db.saveDoc(doc, {
-                success: function() {
-                    // start a timer.
-                    findRecorderTimeoutID = setTimeout(recorderNotFound, 2000, doc, settings);
-                }
-            });
-        }
-
-
-        function stateEvent(doc, state, settings) {
-            console.log(state);
-            if (state == RecordingState.UNKNOWN) {
-                // lets ask for a recorder
-                findRecorder(doc, settings);
-            }
-            if (state == RecordingState.RECORDER_AVAILABLE) {
-                // show the start button
-                recorderFound(doc, settings);
-            }
-            if (state == RecordingState.START_COMPLETE) {
-                startRecordingConfirmed(doc, settings);
-            }
-            if (state == RecordingState.STOP_COMPLETE) {
-                stopRecordingConfirmed(doc, settings);
-            }
-            if (state == RecordingState.RECORDING_COMPLETE) {
-                recordingCompleteConfirmed(doc, settings);
-            }
-        }
-
-
-
-        function init(doc, settings) {
-            // subscribe!
-            //var $changes = settings.db.changes(null, {filter : "couchaudiorecorder/recordings", id : doc._id});
-            settings.db.changes(null, {filter : "couchaudiorecorder/recordings", id : doc._id}).onChange(function (data) {
-                console.log('Got change: ');
-                console.log(data)
-                // get the doc
-                settings.db.openDoc(doc._id, {
-                    success : function(doc) {
-                        console.log("got doc");
-                        var status = recordingStatus(doc);
-                        stateEvent(doc, status, settings);
-                    }
-                })
-            });
-            var state = recordingStatus(doc);
-            stateEvent(doc, state, settings);
-        }
-
-
-
-
-        settings.db.openDoc(settings.recordingDocumentID, {
-            success : function(doc) {
-                init(doc, settings);
-            },
-            error : function() {
-                // create the doc
-                var doc = {
-                    _id : settings.recordingDocumentID,
-                    recordingState : {}
-                }
-                settings.db.saveDoc(doc, {
-                    success : function(){
-                        init(doc, settings);
-                    }
-                })
-
-            }
-
-        })
-
-
-
-
-
-  };
-})( jQuery );
